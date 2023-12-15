096547aa2ca0b66011a5d1bf92ed2b4fbe779fca
your_github_username <your_github_email>
2023-12-15 13:11:34 +0530

    create sotre and dispatch the methods

--- CouterReactRedux/src/components/DisplayCounter.jsx
+++ CouterReactRedux/src/components/DisplayCounter.jsx
@@ -1,7 +1,9 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 const DisplayCounter = () => {
-  return <p className="fs-5 mb-4">Counter current value</p>;
+  const counter = useSelector((state) => state.counter);
+  return <p className="fs-5 mb-4">Counter current value: {counter}</p>;
 };
 
 export default DisplayCounter;
--- CouterReactRedux/src/main.jsx
+++ CouterReactRedux/src/main.jsx
@@ -3,9 +3,13 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Provider } from "react-redux";
+import counterStore from "./store/index.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <Provider store={counterStore}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
